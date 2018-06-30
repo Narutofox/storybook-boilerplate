35c9392d32dc22ad0f943d890325c608736ac0cd
Ivan Čiček <Narutofox@users.noreply.github.com>
2018-06-30 15:50:38 +0200

    footer social media

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import fontAwesomeStyles from './font-awesome.min.css';
 import styles from './index.css';
 
 export default class Footer extends React.Component {
   render() {
-    return <div className={styles.footer}>{this.props.children}</div>;
+    return (
+      <div className={styles.footer}>
+      {this.props.children}
+      <a href="#" className={styles.fa-facebook}></a>
+      <a href="#" className={styles.fa-twitter}></a>
+      <a href="#" className={styles.fa-google}></a>
+      <a href="#" className={styles.fa-linkedin}></a>
+      </div>
+    )
   }
 }
 
