95f9af393fd08b6e7f1df1eb670b939da0a20b9e
Narutofox <Narutofox@users.noreply.github.com>
2018-12-05 13:32:05 +0100

    Icons for RoundIconButton

--- components/card/index.js
+++ components/card/index.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './index.css';
+import info from '../../content/icons/PNG/info.png'
+import plus from '../../content/icons/PNG/plus.png'
+import starFull from '../../content/icons/PNG/star-full.png'
+import starEmpty from '../../content/icons/PNG/star-empty.png'
 
 export default class Card extends React.Component {
+    getFavouriteIcon(isFavourite)
+    {
+        if(isFavourite !== undefined && isFavourite === "true")
+        { 
+            return {starFull};
+        }
+        else
+        {
+            return {starEmpty};
+        }        
+    }
+
     render() 
     {
       return (
           <div className={styles.card}>
               <img src={this.props.imgUrl} style={{maxWidth: '200px',maxHeight: '200px'}}/>
+              <div className={styles.cardStar}>
+                    <RoundIconButton imageLink={this.getFavouriteIcon(this.props.isFavourite)} onClick={this.props.onStarClick}/>
+              </div>
+                <div className={styles.cardActions}>
+                    <RoundIconButton imageLink={info} onClick={this.props.onInfoClick}/>
+                    <RoundIconButton imageLink={plus} onClick={this.props.onPlusClick}/>
+                </div>
               <div className={styles.container}>
                   <h4>{this.props.name}</h4>
                   <p>{this.props.tagline}</p>
               </div>
-              <button onClick={this.props.onStarClick} className={styles.favoriteIcon}>
-                <img src=""/>
-            </button>
-            <button onClick={this.props.onPlusClick} className={styles.plusIcon} >
-                <img src=""/>
-            </button>
           </div>
       )
     }
