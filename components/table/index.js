8ba68b33726269f774a753c70103fa4898afb8e8
Narutofox <Narutofox@users.noreply.github.com>
2018-12-07 15:48:42 +0100

    Work

--- components/table/index.js
+++ components/table/index.js
@@ -4,27 +4,25 @@ import styles from './index.css';
 
 export default class Table extends React.Component {
   render() {
-
-      return (
+    return (
           <table className={styles.table1}>
               <tbody>
               <tr key="RowHeader">
-                {this.props.headerArray.map((header,index) => (<th key={"HeaderContent"+index}>{header}</th>))} 
+                {this.props.headerArray.map((header, index) => (<th key={`HeaderContent${index}`}>{header}</th>))}
               </tr>
-              {this.props.dataArray.map((row,rowIndex) => (
-                  <tr key={"DataRow"+rowIndex}>
-                       {row.map((data,index) => (<td key={"DataRowContent"+rowIndex}>{data}</td>))} 
+              {this.props.dataArray.map((row, rowIndex) => (
+                  <tr key={`DataRow${rowIndex}`}>
+                       {row.map((data, index) => (<td key={`DataRowContent${rowIndex}`}>{data}</td>))}
                   </tr>
               ))}
             </tbody>
         </table>
-      )
-    }
-      
+    );
+  }
 }
 
 Table.propTypes = {
   headerArray: PropTypes.array,
-  dataArray: PropTypes.array,
+  dataArray: PropTypes.array
 };
 
--- stories/main.js
+++ stories/main.js
@@ -7,8 +7,11 @@ import Main from '../components/main';
 import Footer from '../components/footer';
 import Header from '../components/header';
 import Navigation from '../components/navigation';
+import Card from '../components/card';
+import beers from './beers.json';
 
 const mainStory = storiesOf('Main', module);
+const beerCards = beers.map(beer => <Card key={beer.id} imgUrl={beer.image_url} name={beer.name} tagline={beer.tagline}></Card>);
 
 mainStory.add('with text', () => <Main>fgfdgfd</Main>);
 
@@ -16,7 +19,15 @@ mainStory.add('header nav footer', () => (
     <div>
         <Header text="Duff Bear" imgUrl="https://www.maxpixel.net/static/photo/2x/Luck-Letters-Data-Table-Las-Vegas-Numbers-Games-2097929.jpg"></Header>
         <Navigation></Navigation>
-        <Footer>Ivan Čiček &copy; 2018</Footer>
+        <Footer><p>Ivan Čiček &copy; 2018</p></Footer>
     </div>
 ));
 
+mainStory.add('main story', () => (
+    <div>
+        <Header text="Duff Bear" imgUrl="https://www.maxpixel.net/static/photo/2x/Luck-Letters-Data-Table-Las-Vegas-Numbers-Games-2097929.jpg"></Header>
+        <Navigation></Navigation>
+        <Main>{beerCards}</Main>
+        <Footer><p>Ivan Čiček &copy; 2018</p></Footer>
+    </div>
+));
