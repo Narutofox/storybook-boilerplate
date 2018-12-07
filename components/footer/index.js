8f1ae71beca9b64f02d97ab90c95655fda2e6566
Narutofox <Narutofox@users.noreply.github.com>
2018-12-07 14:58:01 +0100

    Https links

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './index.css';
 import { SocialIcon } from 'react-social-icons';
+import styles from './index.css';
+
 
 export default class Footer extends React.Component {
   render() {
     return (
       <div className={styles.footer}>
           {this.props.children}
-          <SocialIcon url="http://twitter.com" />
-          <SocialIcon url="http://google.com" />
-          <SocialIcon url="http://facebook.com" />
-          <SocialIcon url="http://instagram.com" />
-          <SocialIcon url="http://linkedin.com" />
+          <SocialIcon url="https://twitter.com" />
+          <SocialIcon url="https://google.com" />
+          <SocialIcon url="https://facebook.com" />
+          <SocialIcon url="https://instagram.com" />
+          <SocialIcon url="https://linkedin.com" />
       </div>
     )
   }
