afb3bd8e5592e699f1830bc93ad2899ba62cb694
Narutofox <icicek@racunarstvo.hr>
2018-12-28 17:37:23 +0100

    Key for iteration

--- components/navigation/index.js
+++ components/navigation/index.js
@@ -4,31 +4,29 @@ import styles from './index.css';
 
 export default class Navigation extends React.Component {
   render() {
-    
     if (this.props.links == null || this.props.links == undefined) {
       return (
-        <nav className={styles.navigation}> 
+        <nav className={styles.navigation}>
               <a href="#" className={styles.active}>Home</a>
               <a href="#">News</a>
               <a href="#">Contact</a>
               <a href="#">About</a>
           </nav>
-        )
+      );
     }
-    else{
-      return (
-        <nav className={styles.navigation}> 
-          {this.props.links.map(link => (<a href={link.path} className={link.active ? styles.active : undefined}>{link.content}</a>))}                   
+
+    return (
+        <nav className={styles.navigation}>
+         {this.props.links.map((link, i) => {
+               return <a href={link.path} key={i}
+               className={link.active ? styles.active : undefined}>{link.content}</a>; 
+             })}
         </nav>
-      )
-    }
-    
-  
-  
+    );
   }
 }
 
 Navigation.propTypes = {
-  links:PropTypes.array
+  links: PropTypes.array
 };
 
