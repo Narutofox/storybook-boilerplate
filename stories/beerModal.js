5eaf90a8a93789bf8763d523b648bd3f235bfdca
Narutofox <icicek@racunarstvo.hr>
2018-12-30 15:46:13 +0100

    Css and build

--- stories/beerModal.js
+++ stories/beerModal.js
@@ -8,5 +8,5 @@ import BeerModal from '../components/beerModal';
 const beerModalStory = storiesOf('BeerModal', module);
 
 beerModalStory.add('default', () => (
-  <BeerModal show={true} imgUrl="https://images.punkapi.com/v2/keg.png" beerName="Buzz" beerDescription={'A Real Bitter Experience.'} beerId = {1} onClose={action('onClose')} addBeerToCart={action('addBeerToCart')} markBeerAsFavorite={action('markBeerAsFavorite')}></BeerModal>
+  <BeerModal show={true} imgUrl="https://images.punkapi.com/v2/keg.png" beerName="Buzz" beerDescription={'A titillating, neurotic, peroxide punk of a Pale Ale. Combining attitude, style, substance, and a little bit of low self esteem for good measure; what would your mother say? The seductive lure of the sassy passion fruit hop proves too much to resist. All that is even before we get onto the fact that there are no additives, preservatives, pasteurization or strings attached. All wrapped up with the customary BrewDog bite and imaginative twist.'} beerId = {1} onClose={action('onClose')} addBeerToCart={action('addBeerToCart')} markBeerAsFavorite={action('markBeerAsFavorite')}></BeerModal>
 ));
