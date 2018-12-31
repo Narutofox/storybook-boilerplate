63dcdf0b5f59380d647af62b7c2da134603883ed
Narutofox <icicek@racunarstvo.hr>
2018-12-31 16:54:05 +0100

    Table update and new build

--- components/table/index.js
+++ components/table/index.js
@@ -3,18 +3,26 @@ import PropTypes from 'prop-types';
 import styles from './index.css';
 
 export default class Table extends React.Component {
-  renderActions() {
-    if (this.props.actionsArray !== undefined) {
-      return (
-        this.props.actionsArray.map((row, rowIndex) => (
-          row.map((data, index) =>
-            (
-                 <td key={`DataRowContent${rowIndex}`}>
-                   <img src={data.ImageUrl} alt={data.Alt} onClick={data.OnClick} ></img>
-                 </td>
-            ))          
-        ))
-      );
+  constructor(props) {
+    super(props);
+    this.renderTableData = this.renderTableData.bind(this);
+  }
+  // eslint-disable-next-line class-methods-use-this
+  renderTableData(tableBodyData) {
+    if (tableBodyData !== undefined) {
+      if (tableBodyData.value !== '' && tableBodyData.value !== null && tableBodyData.value !== undefined) {
+        return tableBodyData.value;
+      } else if (tableBodyData.image_url !== '' && tableBodyData.image_url !== null && tableBodyData.image_url !== undefined) {
+        const imgStyle = {
+          maxWidth: '10em',
+          maxHeight: '10em'
+        };
+        return (
+          <img src={tableBodyData.image_url} alt={tableBodyData.image_alt} style={ imgStyle }/>);
+      } else if (tableBodyData.inputType !== '' && tableBodyData.inputType !== null && tableBodyData.inputType !== undefined) {
+        return (
+           <input type={tableBodyData.inputType} value={tableBodyData.inputValue} onChange={tableBodyData.inputChange}/>);
+      }
     }
   }
   render() {
@@ -24,13 +32,12 @@ export default class Table extends React.Component {
               <tr key="RowHeader">
                 {this.props.headerArray.map((header, index) => (<th key={`HeaderContent${index}`}>{header}</th>))}
               </tr>
-              {this.props.dataArray.map((row, rowIndex) => (
+              {this.props.tableBodyData.map((row, rowIndex) => (
                   <tr key={`DataRow${rowIndex}`}>
-                       {row.map((data, index) => (<td key={`DataRowContent${rowIndex}`}>{data}</td>))}
-                       {this.renderActions()}
+                    {row.map((data, index) =>
+                    <td key={`DataRowContent${index}`}>{ this.renderTableData(data)} </td>)}
                   </tr>
               ))}
-
             </tbody>
         </table>
     );
@@ -39,7 +46,15 @@ export default class Table extends React.Component {
 
 Table.propTypes = {
   headerArray: PropTypes.array,
-  dataArray: PropTypes.array,
-  actionsArray: PropTypes.array
+  actionsArray: PropTypes.array,
+  renderTableData: PropTypes.func,
+  tableBodyData: PropTypes.shape([{
+    value: PropTypes.string,
+    image_url: PropTypes.string,
+    image_alt: PropTypes.string,
+    inputType: PropTypes.string,
+    inputValue: PropTypes.string,
+    inputChange: PropTypes.func
+  }])
 };
 
